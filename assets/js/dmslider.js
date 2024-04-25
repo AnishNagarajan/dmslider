16c50b45c2fe30e9e6c6488bef3904bf8e29bef6
AnishElon <anishelon@gmail.com>
2024-04-25 22:04:43 +0530

    dmslider

--- assets/js/dmslider.js
+++ assets/js/dmslider.js
@@ -3,14 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const slides = document.querySelectorAll(".slide");
   const sliderBtns = document.querySelectorAll(".slider-btn");
   const slideCount = slides.length;
-  const slideWidth = slides[0].offsetWidth;
+  // const slideWidth = slides[0].offsetWidth;
 
   let currentIndex = 0;
 
   // Function to move the slider to a specific index
   function goToSlide(index) {
     if (index < 0 || index >= slideCount) return;
-    slider.style.transform = `translateX(-${slideWidth * index}px)`;
+    // slider.style.transform = `translateX(-${slideWidth * index}px)`;
     currentIndex = index;
   }
 
